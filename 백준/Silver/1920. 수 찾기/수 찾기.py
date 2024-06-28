d556b503039d1cc6d70784ae887ddd601f8d35ad
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-06-28 23:02:32 +0900

    [Silver IV] Title: 수 찾기, Time: 148 ms, Memory: 50380 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,32 +1,10 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
-Ns = list(map(int, input().split())); Ns.sort()
+Ns = set(map(int, input().split()))
 M = int(input())
 Ms = list(map(int, input().split()))
 
-def binarySearch(t):
-    global N, Ns, M, Ms
-    
-    start = 0; end = N-1;
-    while start <= end:
-        mid = (start+end)//2
-        if Ns[mid] == t:
-            return True
-        elif Ns[mid] > t:
-            end = mid-1
-        else:
-            start = mid+1
-    return 
-
-def Search(): # Ms, Ns
-    global N, Ns, M, Ms
-    
-    for m in Ms:
-        if binarySearch(m): 
-            print(1)
-        else: 
-            print(0)
-
-Search()
+for m in Ms:
+    if m in Ns:
+        print(1)
+    else:
+        print(0)
