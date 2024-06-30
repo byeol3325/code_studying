2d25de79aab9592064e1f0e8c1ab0d00abcdfe6e
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-07-01 07:45:51 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,31 +1,11 @@
 def solution(n,a,b):
-    answer = 0
-    # 1 <-> 2, 3 <-> 4, ...
-    # 다음 라운드 승자1 <-> 승자2 ...
-    all_winers = set() # A와 B는 항상 이김
-    all_winers.add(a)
-    all_winers.add(b)
+    answer = 1
     
-    # 대진표
-    round_info = {} # now_round[번호]: 진짜 번호
-    for i in range(1, n+1):
-        round_info[i] = i
-    
-    round_num = 1
     while True:
-        next_round_info = {} # [번호]: 진짜 번호
-        
-        VS = list(round_info.items())
-        
-        for i in range(len(VS)//2):
-            if VS[2*i][1] in all_winers and VS[2*i+1][1] in all_winers:
-                return round_num
-            
-            if VS[2*i+1][1] in all_winers:
-                next_round_info[i] = VS[2*i+1][1]
-            else:
-                next_round_info[i] = VS[2*i][1]
-        
-        round_info = next_round_info
-        round_num += 1
+        next_a, next_b = (a+1)//2, (b+1)//2
+        if next_a == next_b:
+            return answer
+        else:
+            a, b = next_a, next_b
+            answer += 1
     return answer
