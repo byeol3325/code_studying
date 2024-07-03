b3104f4615e8a6b58df7e2638d7129978d22aacb
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-07-03 17:29:42 +0900

    [level 3] Title: 숫자 타자 대회, Time: 4807.94 ms, Memory: 313 MB -BaekjoonHub

---
+++ 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.py
@@ -0,0 +1,90 @@
+from collections import defaultdict
+from collections import deque
+import heapq as hq
+
+
+LOCATIONS = {} #LOCATIONS[숫자판] = [y,x]
+def set_loc():
+    global LOCATIONS
+    for i in range(1, 10):
+        LOCATIONS[str(i)] = [(i-1)//3, (i-1)%3]
+    LOCATIONS['*'] = [3,0]
+    LOCATIONS['0'] = [3,1]
+    LOCATIONS['#'] = [3,2]
+    return None
+
+def set_w():
+    global LOCATIONS
+    weight_dict = {} # [시작, 끝] = weight
+    for i in range(10): # 0 ~ 9
+        for j in range(i, 10):
+            num1, num2 = i, j
+            loc1, loc2 = LOCATIONS[str(i)], LOCATIONS[str(j)]
+            weight = cal_w(loc1, loc2)
+            weight_dict[(str(num1), str(num2))] = weight
+            weight_dict[(str(num2), str(num1))] = weight
+        loc1, loc2 = LOCATIONS['*'], LOCATIONS[str(i)]
+        weight = cal_w(loc1, loc2)
+        weight_dict[('*', str(i))] = weight
+        weight_dict[(str(i), '*')] = weight
+        
+        loc1, loc2 = LOCATIONS['#'], LOCATIONS[str(i)]
+        weight = cal_w(loc1, loc2)
+        weight_dict[('#', str(i))] = weight
+        weight_dict[(str(i), '#')] = weight
+    return weight_dict
+
+def cal_w(loc1, loc2):
+    dis_y = abs(loc1[0] - loc2[0])
+    dis_x = abs(loc1[1] - loc2[1])
+    
+    diag_mov = min(dis_y, dis_x) # 대각선 이동
+    next_mov = max(dis_y, dis_x) - diag_mov # 상하좌우 이동
+    
+    if dis_y == dis_x and dis_y == 0: # 제자리 눌러야함
+        return 1
+    return diag_mov*3 + next_mov*2
+
+def solution(numbers):
+    global LOCATIONS
+    answer = float("inf")
+    
+    # set number locations
+    set_loc()
+    
+    # set weight
+    weight_dict = set_w()
+    
+    dp = defaultdict(lambda:float("inf")) # dp(L, R, idx) = weight #뒤에 idx 처리해야함
+    # using bfs. compare all ways
+    L = '4'; R = '6'; w = 0; idx = 0
+    n = len(numbers)
+    heap = []
+    hq.heappush(heap, (0, L, R, 0)) # (w, L, R, idx)
+    
+    while heap:
+        w, L, R, idx = hq.heappop(heap)
+        if idx == n: # 가장 작은 weight로 끝까지 다 왔음
+            answer = w
+            break
+        
+        n_num = numbers[idx]
+        n_loc = LOCATIONS[n_num]
+        
+        L_mov = weight_dict[(L, n_num)]
+        R_mov = weight_dict[(R, n_num)]
+        
+        # dp(L, R, idx) = weight #뒤에 idx 처리해야함
+        if min(L_mov, R_mov) == 1: # L이나 R이 그 수 위에 있으면 반드시 그 손가락으로 눌러야함
+            dp[(L, R, idx+1)] = min(w+1, dp[(L, R, idx+1)])
+            hq.heappush(heap, (w+1, L, R, idx+1))
+            continue
+        
+        if dp[(n_num, R, idx+1)] > w+L_mov: # L 이동 (L이 그 숫자 위에 있는게 아님)
+            dp[(n_num, R, idx+1)] = w+L_mov
+            hq.heappush(heap, (w+L_mov, n_num, R, idx+1))
+        if dp[(L, n_num, idx+1)] > w+R_mov: # R 이동 (R이 그 숫자 위에 있는게 아님)
+            dp[(L, n_num, idx+1)] = w+R_mov
+            hq.heappush(heap, (w+R_mov, L, n_num, idx+1))
+        
+    return answer
