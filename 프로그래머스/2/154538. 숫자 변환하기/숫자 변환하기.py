beed53a47efd52a00447a408782c05d24b5b4db4
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-07-05 00:48:51 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -21,12 +21,12 @@ def solution(x, y, n):
         if num == x:
             return cnt
         
-        if (num / 3 == x) or (num / 2 == x) or num - n == x:
+        if (num / 3 == x) or (num / 2 == x) or num - n == x: # 연산 방법 중에 다음에 만들 수 있으면 바로 끝
             return cnt + 1
         
-        if num%3 == 0:
+        if num%3 == 0: # 3으로 나눌 수 있으면 일단 체크
             q.append((num//3, cnt+1))
-        if num%2 == 0:
+        if num%2 == 0: # 2로 나눌 수 있으면 일단 체크
             q.append((num//2, cnt+1))
-        q.append((num-n, cnt+1))
+        q.append((num-n, cnt+1)) # n 빼는 수도 체크
     return -1
