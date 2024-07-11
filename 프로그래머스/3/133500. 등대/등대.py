355eeb6c973cb22ad9f327f4a2e9fec4fe210158
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-07-11 17:52:26 +0900

    [level 3] Title: 등대, Time: 7.37 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.py
+++ 프로그래머스/3/133500. 등대/등대.py
@@ -3,33 +3,28 @@ import sys
 sys.setrecursionlimit(10**6)  # 재귀 깊이 제한 늘리기
 
 def make_graph(lighthouse: list) -> dict:
-    graph = defaultdict(lambda:[])
+    graph = defaultdict(lambda:set())
     for u,v in lighthouse:
-        graph[u-1].append(v-1) # 1번부터 시작이라 -1 해줬음
-        graph[v-1].append(u-1)
+        graph[u-1].add(v-1) # 1번부터 시작이라 -1 해줬음
+        graph[v-1].add(u-1)
     return graph
 
 def solution(n: int, lighthouse:list) -> int:
     answer = 0
     graph = make_graph(lighthouse)
-    #print(graph)
     
-    DP = [[0,0] for _ in range(n)]
     visited = [0] * n
-    
-    def dfs(s: int) -> None:
-        visited[s] = 1
-        for v in graph[s]:
+    def dfs(u):
+        visited[u] = 1
+        on, off = 1, 0
+        
+        for v in graph[u]:
             if visited[v] == 0:
-                dfs(v)
+                on_v, off_v = dfs(v) 
+                on += min(on_v, off_v) # s 등대가 켜지면 연결된 게 켜지든 안 켜지든 상관 없음
+                off += on_v # s가 켜진게 아니라면 연결된게 켜져있어야함
         
-        DP[s][0] += 1 # s가 켜진 경우
-        
-        for v in graph[s]:
-            DP[s][0] += min(DP[v]) # s가 켜진 경우
-            DP[s][1] += DP[v][0] # s가 꺼진 경우, 좌우가 켜져있어야함
-        return None
+        return on, off
     
-    dfs(0)
+    return min(dfs(0))
     
-    return min(DP[0])
