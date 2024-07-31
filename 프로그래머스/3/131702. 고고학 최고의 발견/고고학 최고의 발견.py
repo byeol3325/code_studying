f4597416bc01f7bcc101084ef00d51eab475a649
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-07-31 20:29:34 +0900

    [level 3] Title: 고고학 최고의 발견, Time: 8782.01 ms, Memory: 18.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.py
@@ -0,0 +1,57 @@
+from copy import deepcopy
+
+DIRECTIONS = {-1:[0,0], 0:[-1,0], 1:[0,1], 2:[1,0], 3:[0,-1]} # 0 ~ 3, 12시 ~ 9시. 시계방향
+
+def product(arr: list, n: int):
+    result = []
+    one = []
+    def make(array: list):
+        nonlocal arr, n
+        for i in range(len(arr)):
+            array.append(arr[i])
+            if len(array) == n:
+                result.append(deepcopy(array))
+            else:
+                make(array)
+            array.pop()
+    make(one)
+    return result
+
+def turn(board: list, loc: list, cnt: int):
+    global DIRECTIONS
+    n = len(board)
+    for k, v in DIRECTIONS.items():
+        dr, dc = v
+        nr, nc = loc[0] + dr, loc[1] + dc
+        if (0 <= nr < n) and (0 <= nc < n):
+            board[nr][nc] = (board[nr][nc] + cnt)%4
+    return None
+    
+# 8*8 * 
+def solution(clockHands):
+    answer = 8 * 8 * 3
+    n = len(clockHands)
+    
+    end_board = [[0]*n for _ in range(n)]
+    for one_case in product([0,1,2,3], n): # 첫 줄 돌리는 방법
+        cnt = 0
+        one_board = deepcopy(clockHands)
+        #print(one_board)
+        for j, one_turn in enumerate(one_case):
+            turn(one_board, [0, j], one_turn)
+            cnt += one_turn
+        #print(one_board)
+        #if stop == 2:
+        #    break
+        for i in range(1, n):
+            for j in range(n):
+                needed_turn = (4-one_board[i-1][j])%4
+                if needed_turn != 0:
+                    turn(one_board, [i,j], needed_turn)
+                    cnt += needed_turn
+        
+        if one_board == end_board:
+            answer = min(answer, cnt)
+    
+    # 최소한 조작으로 해결
+    return answer
