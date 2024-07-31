70e9f6b65d81a1e57d396cbd96adfabb35db6291
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-08-01 01:05:11 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.35 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -0,0 +1,95 @@
+from collections import deque
+from copy import deepcopy
+
+def get_info(line):
+    """
+    string을 잘라서 [숫자, 연산자, 숫자 ...] 로 자르기
+    연산자 종류에 operator 넣기
+    """
+    array = deque()
+    num_start = ""
+    operator = set()
+    for e in line:
+        if e == "+" or e == "-" or e == "*":
+            array.append(int(num_start))
+            array.append(e)
+            operator.add(e)
+            num_start = ""
+        else:
+            num_start += e
+    array.append(int(num_start))
+    return array, operator
+
+def do_compute(array, operator):
+    answer = -float("inf")
+    if len(operator) <= 1:
+        result = None
+        while array:
+            one = array.popleft()
+            if result == None:
+                result = one
+            elif one == "-":
+                result -= array.popleft()
+            elif one == "+":
+                result += array.popleft()
+            else: # one == "*":
+                result *= array.popleft()
+        answer = abs(result)
+    else: # len(operator) == 2 or 3
+        cases = permutation(operator)
+        for case in cases:
+            case = deque(case) # 우선순위
+            
+            result = None
+            now_array = deepcopy(array)
+            while case:
+                one_oper = case.popleft()
+                now_array = do_compute_oper(now_array, one_oper)
+            result = now_array[0]
+            answer = max(answer, abs(result))
+    return answer
+
+def do_compute_oper(array: list, oper: str):
+    next_deque = deque()
+    while array:
+        one = array.popleft()
+        if len(next_deque) == 0:
+            next_deque.append(one)
+        elif one == oper:
+            if one == "-":
+                next_deque[-1] -= array.popleft()
+            elif one == "+":
+                next_deque[-1]  += array.popleft()
+            else: # one == "*":
+                next_deque[-1]  *= array.popleft()
+        else: # one != oper
+            next_deque.append(one)
+    return next_deque
+            
+
+def permutation(operator: set):
+    n = len(operator)
+    operator = list(operator)
+    
+    visited = [0]*n
+    results = []
+    def make(one):
+        nonlocal visited, n, operator, results
+        for i in range(n):
+            if visited[i] == 0:
+                visited[i] = 1
+                one.append(operator[i])
+                if len(one) == n:
+                    results.append(deepcopy(one))
+                else:
+                    make(one)
+                visited[i] = 0
+                one.pop()
+        return None
+    make([])
+    return results    
+
+def solution(expression):
+    array, operator = get_info(expression)    
+    answer = do_compute(array, operator)
+    return answer
