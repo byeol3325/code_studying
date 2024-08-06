6511d436c215d89ffa09a2bcb2d6756ee165de78
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-08-06 19:46:30 +0900

    [level 2] Title: N-Queen, Time: 526.55 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,26 +1,16 @@
-def possible(y, x, n, row):
-    for i in range(x):
-        if y == row[i]: # 같은 행에 위치
-            return False
-        if abs(y-row[i]) == x-i: # 같은 대각선
-            return False        
-    return True
-
-def queen(x, n, row):
-    if x == n:
-        return 1
-    count = 0
+def solveNQueens(n):
+    def dfs(row, cols, diags1, diags2):
+        if row == n:
+            return 1
+        count = 0
+        available_positions = (~(cols | diags1 | diags2)) & ((1 << n) - 1)
+        while available_positions:
+            position = available_positions & -available_positions
+            available_positions &= available_positions - 1
+            count += dfs(row + 1, cols | position, (diags1 | position) << 1, (diags2 | position) >> 1)
+        return count
     
-    for y in range(n):
-        if possible(y, x, n, row):
-            row[x] = y
-            count += queen(x+1, n, row)
-    return count
+    return dfs(0, 0, 0, 0)
 
-# 열 col |
 def solution(n):
-    answer = 0
-    row = [0]*n
-    
-    answer = queen(0, n, row)
-    return answer
+    return solveNQueens(n)
