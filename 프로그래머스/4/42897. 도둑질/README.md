b1bc900ed1daf77e3f11ee368a4d44d50e9488a2
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-08-09 13:36:36 +0900

    [level 4] Title: 도둑질, Time: 847.68 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/README.md
+++ 프로그래머스/4/42897. 도둑질/README.md
@@ -1,10 +1,10 @@
 # [level 4] 도둑질 - 42897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897#) 
 
 ### 성능 요약
 
-메모리: 255 MB, 시간: 1501.37 ms
+메모리: 87 MB, 시간: 847.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 13:33:43
+2024년 08월 09일 13:36:33
 
 ### 문제 설명
 
--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,21 +1,21 @@
 def solution(money):
-    answer = 0
     n = len(money)
     
     if n == 1:
         return money[0]
     
-    # 첫 집 털기 => 마지막 집은 털지 않기
-    DP = [[0, 0] for _ in range(n-1)]
-    DP[0][1] = money[0]
-    for i in range(1, n-1):
-        DP[i][0] = max(DP[i-1])
-        DP[i][1] = DP[i-1][0] + money[i]
-    
-    # 첫 집 안 털었음 => 마지막 집은 털지 않기
-    DP_0 = [[0, 0] for _ in range(n)]
-    for i in range(1, n):
-        DP_0[i][0] = max(DP_0[i-1])
-        DP_0[i][1] = DP_0[i-1][0] + money[i]
-    
-    return max(max(DP[-1]), max(DP_0[-1]))
+    # 첫 번째 집을 터는 경우
+    dp1 = [0] * n
+    dp1[0] = money[0]
+    dp1[1] = max(money[0], money[1])
+    for i in range(2, n-1):  # 마지막 집은 포함하지 않음
+        dp1[i] = max(dp1[i-1], dp1[i-2] + money[i])
+        
+    # 첫 번째 집을 털지 않는 경우
+    dp2 = [0] * n
+    dp2[1] = money[1]
+    for i in range(2, n):
+        dp2[i] = max(dp2[i-1], dp2[i-2] + money[i])
+        
+    # 두 경우 중 최댓값을 반환
+    return max(dp1[n-2], dp2[n-1])
